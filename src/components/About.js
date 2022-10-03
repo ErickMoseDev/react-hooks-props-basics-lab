27f5b98bd5e810fba5c8acb2aa2d05470a59875d
ErickMoseDev <erickmongaremose@gmail.com>
2022-10-03 15:09:59 +0300

    Finished Lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React from 'react';
+import Links from './Links';
 
-function About() {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
+function About({ bio, github, linkedin }, e) {
+	return (
+		<div id="about">
+			<h2>About Me</h2>
+
+			{bio && bio.length > 1 ? <p> {bio} </p> : null}
+
+			<img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+			{/* add your <Links /> component here */}
+			<Links github={github} linkedin={linkedin} />
+		</div>
+	);
 }
 
 export default About;
